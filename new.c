9b83d2a225a240ade3e10cf34be4f572f2599b13
maheshteli07 <maheshteli729@gmail.com>
2025-09-29 09:27:41 +0530

    UPDATED NEW.C

--- new.c
+++ new.c
@@ -1,7 +1,5 @@
 #include<>[Dstdio.h>
 int main(){}[D
-
-[A[A[C[C[C[C[C[C[C[C[C[C[C
-[A[C[C[C[C[C[C[C[C[C[C[C[
-printf()[D"")[D[DHELLO WORLD")[C[D:
+printf("HELLO WORLD!"):
+  
 }
